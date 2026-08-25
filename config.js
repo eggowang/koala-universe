a745b64d5008bea9c8734e721e405d97bf55a068
eggowang <318439481+eggowang@users.noreply.github.com>
2026-08-25 16:02:45 +0800

    Connect Koala Universe to Supabase

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // 这里仅允许填写 Supabase Project URL 和 publishable key。
 // publishable key 本来就会公开给浏览器；绝对不要填写 secret/service_role key。
 window.KOALA_CONFIG = {
-  mode: 'demo',
-  supabaseUrl: '',
-  supabasePublishableKey: '',
-  siteUrl: '',
-  vapidPublicKey: '',
+  mode: 'cloud',
+  supabaseUrl: 'https://wnqhzjqfxeobkxsftcex.supabase.co',
+  supabasePublishableKey: 'sb_publishable_htAntmItO9zKCYqgZ1J8Kw_cE6ORBmP',
+  siteUrl: 'https://eggowang.github.io/koala-universe/',
+  vapidPublicKey: 'BAMAAqVsTWleLfGZaVrt8hrGCrN1OjUqkjYGAFDTFxGbC00L00vtGQaBratjxATqtm6kKk12Ngv2-uZSexFL-sI',
 };
