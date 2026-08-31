0db5d5dcbd293c66b8e4505d7ecd64d95a2ff7f7
eggowang <318439481+eggowang@users.noreply.github.com>
2026-08-31 17:02:25 +0800

    Add koala sticker to printable worksheets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'koala-universe-demo-v46';
-const APP_SHELL = ['./', './index.html', './styles.css?v=46', './app.js?v=46', './cloud.js?v=46', './config.js', './manifest.webmanifest', './app-icon.svg', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'koala-universe-demo-v47';
+const APP_SHELL = ['./', './index.html', './styles.css?v=47', './app.js?v=47', './cloud.js?v=47', './config.js', './manifest.webmanifest', './app-icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
