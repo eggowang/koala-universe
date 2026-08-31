fe4fc5c60d3de0c37422b1fe3416d91b08cbb7b7
eggowang <318439481+eggowang@users.noreply.github.com>
2026-08-31 15:36:39 +0800

    Add return link to printable worksheets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'koala-universe-demo-v44';
-const APP_SHELL = ['./', './index.html', './styles.css?v=44', './app.js?v=44', './cloud.js?v=44', './config.js', './manifest.webmanifest', './app-icon.svg', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'koala-universe-demo-v45';
+const APP_SHELL = ['./', './index.html', './styles.css?v=45', './app.js?v=45', './cloud.js?v=45', './config.js', './manifest.webmanifest', './app-icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
